62f8557a77bb88e5a1409e418307b336abe9b9e9
stratakis <cstratak@redhat.com>
2026-08-20 16:49:43 +0200

    Add dtrace worker tag support, tag cstratak's workers (GH-754)
    Co-authored-by: Petr Viktorin <encukou@gmail.com>

--- master/custom/factories.py
+++ master/custom/factories.py
@@ -140,6 +140,7 @@ class UnixBuild(BaseBuild):
             oot_kwargs = {}
         configure_cmd = [configure_cmd, "--prefix", "$(PWD)/target"]
         configure_cmd += self.configureFlags
+        configure_cmd += worker.get_configure_flags(branch)
         self.addStep(
             Configure(command=configure_cmd, **oot_kwargs)
         )
@@ -232,6 +233,7 @@ class UnixInstalledBuild(BaseBuild):
             Configure(
                 command=["./configure", "--prefix", "$(PWD)/target"]
                 + self.configureFlags
+                + worker.get_configure_flags(branch)
             )
         )
 
@@ -449,8 +451,6 @@ class RHEL8Build(UnixBuild):
         "--with-ssl-default-suites=openssl",
         "--without-static-libpython",
         "--with-lto",
-        # Not all workers have dtrace installed
-        # "--with-dtrace",
         "--with-valgrind",
         "--with-system-libmpdec",
     ]
